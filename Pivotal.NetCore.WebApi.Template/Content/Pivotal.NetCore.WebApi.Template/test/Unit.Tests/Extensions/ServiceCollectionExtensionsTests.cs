15ff4bf5d13e50d243d25c70c1d5ad33e3f15aed
Cedric <cedric@MacBook-Pro.localdomain>
2019-06-14 15:12:08 -0500

    upgraded all projects to core 2.2 and nuget packages

--- Pivotal.NetCore.WebApi.Template/Content/Pivotal.NetCore.WebApi.Template/test/Unit.Tests/Extensions/ServiceCollectionExtensionsTests.cs
+++ Pivotal.NetCore.WebApi.Template/Content/Pivotal.NetCore.WebApi.Template/test/Unit.Tests/Extensions/ServiceCollectionExtensionsTests.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Steeltoe.Common.HealthChecks;
 using Steeltoe.Management.Endpoint.Health;
 using System.Linq;
+using FluentAssertions;
 using Xunit;
 
 namespace Pivotal.NetCore.WebApi.Template.Unit.Tests.Extensions
@@ -24,10 +25,9 @@ namespace Pivotal.NetCore.WebApi.Template.Unit.Tests.Extensions
             services.AddActuatorsAndHealthContributors(configuration);
 
             var healthContributors = services.BuildServiceProvider().GetServices<IHealthContributor>().ToList();
+            healthContributors.Should().Contain(x => x.Id.StartsWith("SqlServer"));
 
-            Assert.Contains(healthContributors, s =>s.Id=="SqlServer");
-
-            Assert.Contains(services.BuildServiceProvider().GetServices<IHealthContributor>().ToList(), s => s.Id == "SqlServer");
+            services.BuildServiceProvider().GetServices<IHealthContributor>().Should().Contain(x => x.Id.StartsWith("SqlServer"));
             Assert.NotNull(services.BuildServiceProvider().GetService<HealthEndpoint>());
         }
 
